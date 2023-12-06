d628c171814e6a6b3514b10259d3e597d1704356
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2023-12-06 15:42:10 +0100

    Task: Fixes

--- client/src/components/SignIn/SignIn.jsx
+++ client/src/components/SignIn/SignIn.jsx
@@ -3,23 +3,23 @@ import "./SignIn.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { SignInAction } from "../../actions/authAction";
-import { isAuthenticated } from "../../Utils/isAuthenticated";
 
 function SignIn() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { loading } = useSelector((state) => state.auth);
-  const [auth, setAuth] = useState(isAuthenticated());
   const [signInData, setSignInData] = useState({ email: "", password: "" });
 
   const handlelogin = (e) => {
     e.preventDefault();
-    dispatch(SignInAction(signInData));
+    dispatch(SignInAction(signInData))
+      .then((data) => {
+        if (data.payload.success) navigate("/profile");
+        navigate("/");
+      })
+      .catch((err) => null);
+    setSignInData({ email: "", password: "" });
   };
-  if (auth) {
-    console.log("yes");
-    navigate("/profile");
-  }
 
   return (
     <>
